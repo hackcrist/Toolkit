7ac176a863ae0a627ac487a5b166bf8495f09fa6
Cristhian moises <miniosjuan89@gmail.com>
2025-07-04 08:30:43 -0500

    Actualisado

--- install.sh
+++ install.sh
@@ -4,14 +4,33 @@
 
 clear
 echo "============================================="
-echo "  Instalador de dependencias para la toolkit "
+echo " Instalador de dependencias para la toolkit "
 echo "============================================="
 
 echo "Actualizando paquetes..."
 pkg update && pkg upgrade -y
 
-echo "Instalando PHP, nmap, sqlmap, hydra, figlet y lolcat..."
-pkg install php nmap sqlmap hydra figlet lolcat -y
+echo "Instalando PHP..."
+pkg install php -y
 
-echo "Instalación completa. Puedes ejecutar bash toolkit.sh"
-echo "Síguenos en TikTok para más contenido: https://www.tiktok.com/@ethicalcore?_t=ZT-8xhbDw3hG62&_r=1"
+echo "Instalando Nmap..."
+pkg install nmap -y
+
+echo "Instalando SQLMap..."
+pkg install sqlmap -y
+
+echo "Instalando Hydra..."
+pkg install hydra -y
+
+echo "Instalando Figlet..."
+pkg install figlet -y
+
+echo "Instalando Lolcat..."
+pkg install lolcat -y
+
+echo "Instalación completa."
+
+echo "Abriendo tu canal de TikTok..."
+termux-open-url https://www.tiktok.com/@ethicalcore?_t=ZT-8xhbDw3hG62&_r=1
+
+echo "Puedes ejecutar: bash toolkit.sh"
